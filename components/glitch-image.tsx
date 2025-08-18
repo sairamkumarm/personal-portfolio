3894f30cbaaf6d52ae6168b5e2ff890ecdcacfbe
Sairamkumar M <88662373+sairamkumarm@users.noreply.github.com>
2025-08-18 23:22:17 +0530

    Updated glitch-image.tsx

--- components/glitch-image.tsx
+++ components/glitch-image.tsx
@@ -41,7 +41,7 @@ export function GlitchImage({
       <img
         src={src || "/placeholder.svg"}
         alt={alt}
-        className=" w-full h-full object-scale-down "
+        className=" object-contain "
       />
       {shouldGlitchIn && !debugMode && (
         <div className="absolute inset-0 glitch-overlay z-10" />
