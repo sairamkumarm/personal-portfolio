f00f9c941619bf21424269e4873a03e22fb39b5c
Sairamkumar M <88662373+sairamkumarm@users.noreply.github.com>
2025-08-18 20:14:21 +0530

    Updated operator-profile.tsx

--- components/operator-profile.tsx
+++ components/operator-profile.tsx
@@ -262,7 +262,7 @@ export function OperatorProfile({ data }: OperatorProfileProps) {
                         <a key={`link-${index}`}
                         href={link.url}
                         target="_blank"
-                        className="flex items-center"
+                        className=""
                         rel="noopener noreferrer">
                           <GlitchComponent
                             bracket
