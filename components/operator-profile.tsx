901bfec0feb877c6d693cb929e630206945f93b5
Sairamkumar M <88662373+sairamkumarm@users.noreply.github.com>
2025-08-18 18:22:33 +0530

    Updated operator-profile.tsx

--- components/operator-profile.tsx
+++ components/operator-profile.tsx
@@ -182,7 +182,7 @@ export function OperatorProfile({ data }: OperatorProfileProps) {
                     </div>
 
                     {/* Location - Responsive */}
-                    <div className="text-xs sm:text-sm text-theme-secondary">
+                    {/* <div className="text-xs sm:text-sm text-theme-secondary">
                       <GlitchComponent
                         delay={calculateFieldDelay(data.personal_info.renderIndex, 2)}
                         shouldStart={textPhaseActive}
@@ -191,7 +191,7 @@ export function OperatorProfile({ data }: OperatorProfileProps) {
                       >
                         {data.personal_info.location}
                       </GlitchComponent>
-                    </div>
+                    </div> */}
 
                     {/* Contact - Responsive */}
                     <div className="text-xs break-all sm:text-sm text-theme-secondary">
