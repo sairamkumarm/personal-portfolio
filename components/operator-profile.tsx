e858a75b6e07d7f9057c7d32d4b9d614fdc70d8b
Sairamkumar M <88662373+sairamkumarm@users.noreply.github.com>
2025-08-18 23:12:35 +0530

    Updated operator-profile.tsx

--- components/operator-profile.tsx
+++ components/operator-profile.tsx
@@ -253,7 +253,7 @@ export function OperatorProfile({ data }: OperatorProfileProps) {
                         delay={calculateFieldDelay(data.personal_info.renderIndex, 4)}
                         shouldStart={textPhaseActive}
                         debugMode={DEBUG_MODE} 
-                        className="max-h-[190px] w-auto h-full object-contain "
+                        className="max-h-[190px] w-auto h-auto object-contain "
                       />
                     </div>
                     {/* Links - Show only on phones */}
