a35c3321230280f4b4815f9095c3a6960f22ff8c
Sairamkumar M <88662373+sairamkumarm@users.noreply.github.com>
2025-08-18 23:33:24 +0530

    Updated operator-profile.tsx

--- components/operator-profile.tsx
+++ components/operator-profile.tsx
@@ -244,7 +244,7 @@ export function OperatorProfile({ data }: OperatorProfileProps) {
                   </div>
 
                   {/* Col 2: Photo + Links (phones) / Photo only (lg+) */}
-                  <div className=" flex flex-col flex-end items-center gap-1 justify-center">
+                  <div className=" flex flex-col items-center gap-1 justify-end">
                     {/* Photo */}
                     <div className=""> 
                       <GlitchImage
